4d5718d3f8498543b862fdf3d9c870781d91d26a
surianto <suriantosag04@gmail.com>
2026-09-15 13:25:54 +0800

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,3 @@
-// Isi 2 nilai ini dari Supabase > Project Settings > API.
-// ANON/PUBLISHABLE KEY aman ditaruh di frontend selama RLS sudah benar.
 window.SUPABASE_URL = 'https://gefbibbmqvphpwmaanzv.supabase.co';
-window.SUPABASE_ANON_KEY = 'sb_publishable_RhTlOQcwdcNJwnuqdmYMvA_2etFZF_f
-';
-window.ADMIN_EMAIL = 'bismillah 2026!';
+window.SUPABASE_ANON_KEY = 'sb_publishable_RhTlOQcwdcNJwnuqdmYMvA_2etFZF_f';
+window.ADMIN_EMAIL = 'admin@penyuluh.local';
