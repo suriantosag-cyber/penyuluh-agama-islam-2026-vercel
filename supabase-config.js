ab9cf254092c9d3900eaf7562fc570044e02d1d1
surianto <suriantosag04@gmail.com>
2026-09-15 13:13:47 +0800

    Update Supabase configuration values

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,6 @@
 // Isi 2 nilai ini dari Supabase > Project Settings > API.
 // ANON/PUBLISHABLE KEY aman ditaruh di frontend selama RLS sudah benar.
-window.SUPABASE_URL = 'https://YOUR-PROJECT.supabase.co';
-window.SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_OR_PUBLISHABLE_KEY';
-window.ADMIN_EMAIL = 'admin@penyuluh.local';
+window.SUPABASE_URL = 'https://gefbibbmqvphpwmaanzv.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_RhTlOQcwdcNJwnuqdmYMvA_2etFZF_f
+';
+window.ADMIN_EMAIL = 'bismillah 2026!';
